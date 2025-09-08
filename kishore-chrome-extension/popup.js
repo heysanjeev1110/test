b21a3565abf5f8f52ac38b6f931308721af1c42c
heysanjeev1110 <117549283+heysanjeev1110@users.noreply.github.com>
2025-09-08 15:00:19 +0530

    Update popup.js

--- kishore-chrome-extension/popup.js
+++ kishore-chrome-extension/popup.js
@@ -1,7 +1,7 @@
 document.getElementById('play-music').addEventListener('click', function() {
   // Open a random Kishore Kumar song or playlist in a new tab
   const kishoreSongs = [
-    'https://www.youtube.com/watch?v=Zkq0gVjv6kY',
+    'https://www.youtube.com/watch?v=c_my5B15ENU&list=RDc_my5B15ENU&start_radio=1',
     'https://www.youtube.com/watch?v=rBqmGVp_XKw&list=RDrBqmGVp_XKw&start_radio=1',
     'https://www.youtube.com/watch?v=j7TM2ccOGbU&list=PLUOEf-vLOCSkxWY5z9cjS4OT3oZ9D8suk',
     'https://www.youtube.com/watch?v=x50-kXC54yU&list=RDx50-kXC54yU&start_radio=1',
