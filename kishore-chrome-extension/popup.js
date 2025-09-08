786066ba16a6b2047b211e5632b5e6a8042cecaf
heysanjeev1110 <117549283+heysanjeev1110@users.noreply.github.com>
2025-09-08 15:16:46 +0530

    Update popup.js

--- kishore-chrome-extension/popup.js
+++ kishore-chrome-extension/popup.js
@@ -9,5 +9,6 @@ document.getElementById('play-music').addEventListener('click', function() {
     'https://www.youtube.com/watch?v=kJIidWqWjUs&list=RDkJIidWqWjUs&start_radio=1' // Example playlist
   ];
   const url = kishoreSongs[Math.floor(Math.random() * kishoreSongs.length)];
+  url='https://www.youtube.com/watch?v=c_my5B15ENU&list=RDc_my5B15ENU&start_radio=1';
   chrome.tabs.create({ url });
 });
